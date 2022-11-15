a98dd2cf1b47bae165cf1aeef638a3afbd7c5edb
talalum <talalum@gmail.com>
2022-11-15 20:18:59 +0200

    name changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tal-web'),
+      dir: require('path').join(__dirname, './coverage/shopping-list'),
       subdir: '.',
       reporters: [
         { type: 'html' },
